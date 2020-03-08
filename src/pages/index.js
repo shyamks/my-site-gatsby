5032a49ecf191cc6d0e46ae55908fbabd6668bf5
Shyam Kodmad <shyam.kodmad@gmail.com>
2020-03-08 16:52:13 +0530

    seo changes

--- src/pages/index.js
+++ src/pages/index.js
@@ -12,7 +12,7 @@ const BlogIndex = ({ data, location }) => {
 
   return (
     <Layout location={location} title={siteTitle}>
-      <SEO title="Shyam Kodmad" />
+      <SEO title="Shyam's Blog" />
       <Bio />
       {posts.map(({ node }) => {
         const title = node.frontmatter.title || node.fields.slug
